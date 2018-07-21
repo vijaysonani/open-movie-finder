9f9f35f8d9eca527094767ebd7d039faebf1fdad
Vijay Sonani <vijay.sonani@cerner.com>
2018-07-20 21:52:50 -0500

    reseting master

--- client/src/App.js
+++ client/src/App.js
@@ -2,34 +2,6 @@ import React, { Component } from 'react';
 import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react';
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {}
-    // this.getDrinks = this.getDrinks.bind(this)
-    // this.getDrink = this.getDrink.bind(this)
-  }
-  // componentDidMount () {
-  //   this.getDrinks()
-  // }
-  // fetch (endpoint) {
-  //   return new Promise((resolve, reject) => {
-  //     window.fetch(endpoint)
-  //     .then(response => response.json())
-  //     .then(json => resolve(json))
-  //     .catch(error => reject(error))
-  //   })
-  // }
-  // getDrinks () {
-  //   this.fetch('api/drinks')
-  //     .then(drinks => {
-  //       this.setState({drinks: drinks})
-  //       drinks[0] && this.getDrink(drinks[0].id)
-  //     })
-  // }
-  // getDrink (id) {
-  //   this.fetch(`api/drinks/${id}`)
-  //     .then(drink => this.setState({drink: drink}))
-  // }
   render () {
     return (
       <Container>
@@ -37,7 +9,7 @@ class App extends Component {
         <Divider hidden />
         <Divider hidden />
         <Divider hidden />
-        <Container textAlign='center'><Header size='large'>Hello beautiful!!</Header></Container>
+        <Container textAlign='center'><Header size='large'>Hello, beautiful!!</Header></Container>
       </Container>
     );
   }
